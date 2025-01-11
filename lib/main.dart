3b23532770606c29fd9c33f67cabbd927101feda
niranjandahal <niranjandahal76@gmail.com>
2025-01-12 01:33:50 +0545

    chnages for productoin view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,43 @@
+// import 'package:flutter/material.dart';
+// // import 'UI/delivery.dart';
+// import 'UI/pagemain.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(useMaterial3: false),
+
+//       home: MyHomePage(),
+//       // ),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.green.shade200,
+//       body: pagemain(),
+//       // body: DeliveryUi(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-// import 'UI/delivery.dart';
 import 'UI/pagemain.dart';
 
 void main() {
@@ -12,10 +50,79 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: false),
-
+      builder: (context, child) {
+        return ForcedMobileView(
+            child: child!); // Wrap the child with ForcedMobileView
+      },
       home: MyHomePage(),
-      // ),
     );
+  }
+}
+
+class ForcedMobileView extends StatelessWidget {
+  final Widget child;
+
+  const ForcedMobileView({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    // Define Pixel 7 dimensions
+    const double mobileWidth = 450; // Pixel 7 width
+    const double mobileHeight = 900; // Pixel 7 height
+
+    // Get the current screen size
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    // Force mobile dimensions if the screen is larger than mobile size
+    if (screenWidth > mobileWidth || screenHeight > mobileHeight) {
+      return Column(
+        children: [
+          const Text(
+            'Zoom out browser to see full screen',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'All features might not work in web view',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 100),
+          Center(
+            child: Container(
+              width: mobileWidth,
+              height: mobileHeight,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5), blurRadius: 10),
+                ],
+              ),
+              child: MediaQuery(
+                data: MediaQueryData(
+                  size: const Size(mobileWidth, mobileHeight),
+                  devicePixelRatio: MediaQuery.of(context).devicePixelRatio,
+                  padding: MediaQuery.of(context).padding,
+                  viewInsets: MediaQuery.of(context).viewInsets,
+                ),
+                child: child,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    // Default rendering for mobile-sized screens
+    return child;
   }
 }
 
@@ -31,8 +138,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green.shade200,
-      body: pagemain(),
-      // body: DeliveryUi(),
+      body: pagemain(), // Your main page widget
     );
   }
 }
